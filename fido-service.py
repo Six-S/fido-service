4fb64f199dc95dfcafb03a3e49170b1e1de95510
Six-S <brenlink@gmail.com>
2019-07-31 00:25:08 -0600

    Addition of a couple of util functions as well as moving our connection logic and making it more robust. This version has an issue that causes an unrecoverable error after our connection request has been sent.

--- fido-service.py
+++ fido-service.py
@@ -1,8 +1,11 @@
 import sys, json
 import socket
 
+#LETS MAKE A LOG FILE, OKAY?
+
 HOST, PORT = '10.0.0.81', 3000
-localIP = ''
+address = ''
+connection = ''
 #data = 'Request Connect'
 
 class MessageHandler():
@@ -10,17 +13,65 @@ class MessageHandler():
     def __init__(self):
         print 'Starting request handler'
 
+    #this will handle the incoming responses from our dispatch server
     def parseMessage(self, message):
         print message
 
+    #this checks the type of our response to make sure that we can run json.loads on it.
+    #if we return false, we'll exit non-zero, so checking that first will help us handle this error.
+    def typeCheck(self, to_check):
+        if isinstance(to_check, basestring):
+            return True
+        else:
+            return False
+
+    #this logic runs on init in order to make sure the recieving end exists, and that
+    #it's ready to recieve messages. We also use this function to get our connection information.
+    def connect(self, socket):
+        data = 'Request Connect'
+        sock.sendall(data + '\n')
+
+        recieved = sock.recv(1024)
+
+        global connection, address
+        if self.typeCheck(data):
+            data = json.loads(recieved)
+            print data
+            if data['type'] == 'GoodBye':
+                return True
+            elif data['result'] == 'Approved':
+                address = data['requestingip']
+                return False
+            elif data['result'] == 'Active':
+                address = data['requestingip']
+                return False
+            else:
+                print 'an unexpected error occured. Quitting...'
+                return True
+
+    #sets the socket object to a variable, sends a connection request to the server
+    def setSockData(self, socket):
+        self.socket = socket
+        self.connect(socket)
+
+
 if __name__ in '__main__':
 
+    #set up the socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
+        #connect the socket
         sock.connect((HOST, PORT))
 
+        #set up our message handler
         handle = MessageHandler()
+
+        #save our socket object to our messagehandler class, then run connect.
+        #handle.setSockData(sock)
+
+        #start the backbone of our service.
+        #loop and send messages until we can't anymore.
         disconnect = False
         while not disconnect:
             data = raw_input('> ')
@@ -28,19 +79,19 @@ if __name__ in '__main__':
 
             recieved = sock.recv(1024)
 
-            data = json.loads(recieved)
-            if data['type'] == 'GoodBye':
-                disconnect = True
+            #send current IP and time with each message
+
+            if handle.typeCheck(recieved) and recieved != '':
+                print recieved
+                data = json.loads(recieved)
+                if data['type'] == 'GoodBye':
+                    disconnect = True
+                else:
+                    handle.parseMessage(data)
             else:
-                handle.parseMessage(data)
+                print 'We did not get a string. We should not be here.'
+                print recieved
 
     finally:
-        #print 'An error occured, and the socket either could not be opened, or a message could not be sent.'
+        #we should also try and send a goodbye from here if we can.
         sock.close()
-        #raise SystemExit
-
-    #finally:
-        #sock.close()
-
-    print 'Sent: {}'.format(data)
-    print 'Recieved: {}'.format(recieved)
